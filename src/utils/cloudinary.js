e206996513b50fdc7ad28a3ecfa872f9a027661b
first <vk9444005.in>
2026-09-10 19:30:31 +0530

    configuration

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,31 @@
+import {v2 as cloudinary} from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async (localFilePath) =>{
+    try{
+        if(!localFilePath) return null
+        // upload the file on cloudinary
+        const response =cloudinary.uploader.upload(localFilePath,{
+            resource_type: "auto"
+        })
+        //file uploaded successfully
+        console.log("File uploaded successfully oon cloudinary",(await response).url);
+        return response
+    }catch(error){
+        fs.unlinkSync(localFilePath)
+    }
+}
+
+cloudinary.v2.uploader
+.upload("dog.mp4", {
+  resource_type: "video", 
+  public_id: "my_dog",
+  overwrite: true, 
+  notification_url: "https://mysite.example.com/notify_endpoint"})
+.then(result=>console.log(result));
